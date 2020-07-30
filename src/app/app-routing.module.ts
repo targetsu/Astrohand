023501a84a9abcc17bb7d0d8527f6be5b41443e1
praveen <medegarpk@gmail.com>
2020-07-30 08:22:01 +0530

    'pdn'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'admin',
+    loadChildren:'./modules/admin/admin.module#AdminModule'
+  },
+ {
+   path:'astro',
+   loadChildren:'./modules/astrologer/astrologer.module#AstrologerModule',
+ },
+ {
+   path:'user',
+   loadChildren:'./modules/user/user.module#UserModule',
+ }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
